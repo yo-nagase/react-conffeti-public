51c943b747dea2a04aa2d97ce2c33b5bb57ed87d
Nagase Yoshimasa <yoshimasa_nagase@tsyscom.co.jp>
2024-01-18 13:52:10 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
       <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>⭐️tart editing to see some magsssic happen :)</p>
     </div>
   );
 };
